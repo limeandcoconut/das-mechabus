e574f699683151cf29bbbe4c0d4ee9bf6197654a
Jacob Smith <js@jacobsmith.tech>
2020-03-23 16:26:59 -0700

    Change ecosystem name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: 'das',
+    name: 'mechabus',
     script: 'server/serve.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
