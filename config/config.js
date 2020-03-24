ec64adf606b5d19cb78b036f318303143b8bb1a8
Jacob Smith <js@jacobsmith.tech>
2020-03-23 19:21:01 -0700

    Distinguish between internal or external prod host

--- config/config.js
+++ config/config.js
@@ -1,13 +1,19 @@
 
 // Keep in sync with utils
 const isProd = process.env.NODE_ENV === 'production'
+const isExternal = process.env.EXTERNAL_MODE === true || process.env.EXTERNAL_MODE === 'true'
 const useProd = isProd || process.env.USE_PROD_API
+
+let host = 'localhost'
+if (useProd) {
+  host = isExternal ? 'mechabus.jacobsmith.tech' : '10.0.0.4'
+}
 
 module.exports = {
   // Update this url in ./meta.config.js too. It saves a notable chunk from the bundles by doing it manually.
   productionHost: 'https://mechabus.jacobsmith.tech',
   frontendPort: 3015,
-  host: useProd ? 'mechabus.jacobsmith.tech' : 'localhost',
+  host,
   httpProtocol: useProd ? 'https://' : 'http://',
   wsProtocol: useProd ? 'wss://' : 'ws://',
   wsPort: ':3535',
