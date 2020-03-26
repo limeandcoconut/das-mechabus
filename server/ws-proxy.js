d4f2513535bfb102c335ef79a83b84481b719924
Jacob Smith <js@jacobsmith.tech>
2020-03-25 17:43:07 -0700

    Try different proxy

--- server/ws-proxy.js
+++ server/ws-proxy.js
@@ -1,12 +1,29 @@
-const httpProxy = require('http-proxy')
-const fs = require('fs')
+const url = require('url')
+const WebSocket = require('ws')
+const HttpsProxyAgent = require('https-proxy-agent')
 
-httpProxy.createServer({
-  target: 'ws://107.77.97.117:3535',
-  ssl: {
-    key: fs.readFileSync('./mechabus-key.pem', 'utf8'),
-    cert: fs.readFileSync('./mechabus-cert.pem', 'utf8'),
-  },
-  ws: true,
-  secure: true,
-}).listen(3535)
+// HTTP/HTTPS proxy to connect to
+const proxy = process.env.http_proxy || 'http://localhost:3535'
+console.log('using proxy server %j', proxy)
+
+// WebSocket endpoint for the proxy to connect to
+const endpoint = 'wss://107.77.97.117:3535'
+console.log('attempting to connect to WebSocket %j', endpoint)
+
+// create an instance of the `HttpsProxyAgent` class with the proxy server information
+const options = url.parse(proxy)
+
+const agent = new HttpsProxyAgent(options)
+
+// finally, initiate the WebSocket connection
+const socket = new WebSocket(endpoint, { agent: agent })
+
+socket.on('open', function() {
+  console.log('"open" event!')
+  socket.send('hello world')
+})
+
+socket.on('message', function(data, flags) {
+  console.log('"message" event! %j %j', data, flags)
+  socket.close()
+})
